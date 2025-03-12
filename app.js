e0291f16caa7a066a729d76caf8ee61826d4fe8f
John Miguel <jonhmiguel132@gmail.com>
2025-03-12 15:01:19 -0300

    configurar repositório do github

---
+++ app.js
@@ -0,0 +1,8 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+const amigos = []; //lista de amigos
+
+function adicionar_amigo() {
+    let entrada = document.getElementById("amigo");
+    let nome = entrada.value
+}
