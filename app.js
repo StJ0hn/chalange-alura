41a0d9bc45c22836192291213bc01835c5c78b98
John Miguel <jonhmiguel132@gmail.com>
2025-03-12 15:25:22 -0300

    criar função de adicionar amigos na lista,inicializar inserção da função de listagem de amigos

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
-const amigos = []; //lista de amigos
+let amigos = []; //lista de amigos
 
+//função para adicionar amigo na lista de amigos que vão ser sorteados
 function adicionar_amigo() {
-    let entrada = document.getElementById("amigo");
-    let nome = entrada.value
+    const entrada = document.getElementById('amigo');
+    const nome = entrada.value.trim();
+
+    if (nome !== "") { //se o input não estiver vazio
+        amigos.push(nome); //adiciona o nome do amigo na lista
+        mostrar_amigos();
+        entrada.value = "" //limpando o campo de entrada
+    }
+    else{
+        alert("Digite o nome de algum amigo antes de adicioná-lo na lista.");
+    }
+}
+
+
+function mostrar_amigos() {
+    if (amigos.length <= 1) {
+        alert("Para efetuar o sorteio, é necessário adicionar pelo menos dois amigos.")
+    }
 }
